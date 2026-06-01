aa14f1600be3920a306ece2b8d9ad5b270f5ed99
Viper-AI-V01 <lovysharma0289@gmail.com>
2026-06-01 15:03:39 +0530

    FinalCommit

--- src/utils.py
+++ src/utils.py
@@ -5,15 +5,16 @@ import numpy as np
 from src.exception import CustomException
 from src.logger import logging
 from sklearn.model_selection import GridSearchCV
+import pickle
 
-import dill
+
 
 def save_object(file_path,obj):
     try:
         dir_path = os.path.dirname(file_path)
         os.makedirs(dir_path,exist_ok=True)
         with open(file_path,'wb') as file_obj:
-            dill.dump(obj,file_obj)
+            pickle.dump(obj,file_obj)
             
     except Exception as e:
         raise CustomException(e,sys) # type: ignore
@@ -43,3 +44,11 @@ def evaluate_model(X_train,X_test,y_train,y_test,models,metrics,param):
     except Exception as e:
         logging.error(sys)
         raise CustomException(e,sys)# type: ignore
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys) # type: ignore
